6023c059df189ccbee14852aefdd8afb63b1c3ff
Amit Pardeshi <amit.pardeshi2000@gmail.com>
2025-07-25 23:55:41 +0530

    demo

--- demo/src/main/java/com/hellloworld/demo/HelloWorldApplication.java
+++ demo/src/main/java/com/hellloworld/demo/HelloWorldApplication.java
@@ -8,7 +8,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.println("This is my First Deploy Application Using Github Actions.");
+		System.out.println("This is my First  Application Using Github Actions.");
 	}
 
 }
